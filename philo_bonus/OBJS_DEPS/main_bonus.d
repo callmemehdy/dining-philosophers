de3f64aa620626287625597343851ba24dcf9fe3
Mehdi El Akary <rock.mido15@gmail.com>
2024-07-29 16:56:26 +0100

    yesterday

--- philo_bonus/OBJS_DEPS/main_bonus.d
+++
@@ -1,2 +0,0 @@
-OBJS_DEPS/main_bonus.o: SRCS/main_bonus.c \
-  SRCS/../headers/philosophers_bonus.h
--- philo_bonus/OBJS_DEPS/monitor_bonus.d
+++
@@ -1,2 +0,0 @@
-OBJS_DEPS/monitor_bonus.o: SRCS/monitor_bonus.c \
-  SRCS/../headers/philosophers_bonus.h
